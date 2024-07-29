58f8ac4b7f750ba3c27be1c12a90cbe4483dd016
A.A <37187773+bowale01@users.noreply.github.com>
2024-07-29 11:08:59 +0200

    Update main.go

--- Codebug_fix/server_new/main.go
+++ Codebug_fix/server_new/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"math"
 	"math/rand"
 	"net/http"
 	"sync"
@@ -43,26 +42,31 @@ func main() {
 	}
 	go rc.start(ctx)
 	err := http.ListenAndServe(":3000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer r.Body.Close()
 		d := &struct {
 			Entrance string `json:"entrance"`
 		}{}
-		json.NewDecoder(r.Body).Decode(d)
+		if err := json.NewDecoder(r.Body).Decode(d); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
 		isVIP := rand.Float64() >= 0.7
 
 		rc.idMu.Lock()
-		rider := &rider{id: rc.nextId, entrance: d.Entrance, name: namesgenerator.GetRandomName(0), vipStatus: isVIP}
+		r := &rider{id: rc.nextId, entrance: d.Entrance, name: namesgenerator.GetRandomName(0), vipStatus: isVIP}
 		rc.nextId++
 		rc.idMu.Unlock()
 
 		rc.rideQueueMu.Lock()
-		if rider.vipStatus {
-			rc.rideQueue = append([]*rider{rider}, rc.rideQueue...)
+		if r.vipStatus {
+			rc.rideQueue = append([]*rider{r}, rc.rideQueue...)
 		} else {
-			rc.rideQueue = append(rc.rideQueue, rider)
+			rc.rideQueue = append(rc.rideQueue, r)
 		}
+		queueSize := len(rc.rideQueue)
 		rc.rideQueueMu.Unlock()
-		log.Printf("Entrance %s: %s entered the queue. Size: %d\n", d.Entrance, rider.name, len(rc.rideQueue))
+
+		log.Printf("Entrance %s: %s entered the queue. Size: %d\n", d.Entrance, r.name, queueSize)
 	}))
 	if err != nil {
 		panic(err)
@@ -75,27 +79,43 @@ func (rc *rollercoaster) start(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		default:
-			rc.rideMu.Lock()
 			rc.rideQueueMu.Lock()
-			for i := 0; i < numberOfCars*carCapacity && len(rc.rideQueue) > 0; i++ {
-				r := rc.rideQueue[0]
-				car := int(math.Floor(float64(i) / carCapacity)) + 1
+			for i := 0; i < numberOfCars*carCapacity && i < len(rc.rideQueue); i++ {
+				rc.rideQueueMu.Unlock()
+
+				rc.rideMu.Lock()
+				r := rc.rideQueue[i]
+				rc.ride = append(rc.ride, r)
+				car := i / carCapacity
 				carSeat := i % carCapacity
 				rc.seatRider(r, car, carSeat)
-				rc.rideQueue = rc.rideQueue[1:]
+				rc.rideMu.Unlock()
+
+				rc.rideQueueMu.Lock()
 			}
+			rc.rideQueue = rc.rideQueue[min(numberOfCars*carCapacity, len(rc.rideQueue)):]
 			rc.rideQueueMu.Unlock()
+
 			log.Println("Ride: started")
 			time.Sleep(rideDuration * time.Millisecond)
 			log.Println("Ride: finished")
+
+			rc.rideMu.Lock()
+			rc.ride = rc.ride[:0] // Clear the ride
 			rc.rideMu.Unlock()
 		}
 	}
 }
 
 func (rc *rollercoaster) seatRider(r *rider, car, carSeat int) {
-	log.Printf("Ride: %s entering the car %d in seat %d\n", r.name, car, carSeat)
+	log.Printf("Ride: %s entering the car %d in seat %d\n", r.name, car+1, carSeat+1)
 	time.Sleep(400 * time.Millisecond)
-	rc.ride = append(rc.ride, r)
-	log.Printf("Ride: %s entered the car %d in seat %d\n", r.name, car, carSeat)
+	log.Printf("Ride: %s entered the car %d in seat %d\n", r.name, car+1, carSeat+1)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
